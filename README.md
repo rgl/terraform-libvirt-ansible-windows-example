14d503b711c39899b263247decf55afe3a4a8754
Rui Lopes <rgl@ruilopes.com>
2021-04-24 15:09:50 +0100

    init

---
+++ README.md
@@ -0,0 +1,133 @@
+# Usage (Ubuntu 20.04 host)
+
+Install Terraform:
+
+```bash
+wget https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_amd64.zip
+unzip terraform_0.15.0_linux_amd64.zip
+sudo install terraform /usr/local/bin
+rm terraform terraform_*_linux_amd64.zip
+```
+
+Install the [terraform libvirt provider](https://github.com/dmacvicar/terraform-provider-libvirt):
+
+```bash
+wget https://github.com/dmacvicar/terraform-provider-libvirt/releases/download/v0.6.3/terraform-provider-libvirt-0.6.3+git.1604843676.67f4f2aa.Ubuntu_20.04.amd64.tar.gz
+tar xf terraform-provider-libvirt-0.6.3+git.1604843676.67f4f2aa.Ubuntu_20.04.amd64.tar.gz
+install -d ~/.local/share/terraform/plugins/registry.terraform.io/dmacvicar/libvirt/0.6.3/linux_amd64
+install terraform-provider-libvirt ~/.local/share/terraform/plugins/registry.terraform.io/dmacvicar/libvirt/0.6.3/linux_amd64/
+rm terraform-provider-libvirt terraform-provider-libvirt-*.amd64.tar.gz
+```
+
+Or install it from source:
+
+```bash
+sudo apt-get install -y libvirt-dev
+git clone https://github.com/dmacvicar/terraform-provider-libvirt.git
+cd terraform-provider-libvirt
+make
+install -d ~/.local/share/terraform/plugins/registry.terraform.io/dmacvicar/libvirt/0.6.3/linux_amd64
+install terraform-provider-libvirt ~/.local/share/terraform/plugins/registry.terraform.io/dmacvicar/libvirt/0.6.3/linux_amd64/
+cd ..
+```
+
+Install Ansible:
+
+```bash
+ansible_version='2.9.20'        # see https://pypi.org/project/ansible/
+ansible_lint_version='4.3.7'    # see https://pypi.org/project/ansible-lint/
+# see https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-with-pip
+sudo apt-get install -y --no-install-recommends python3-pip python3-venv
+rm -rf .ansible-venv
+python3 -m venv .ansible-venv
+source .ansible-venv/bin/activate
+# NB this pip install will display several "error: invalid command 'bdist_wheel'"
+#    messages, those can be ignored.
+python3 -m pip install ansible==$ansible_version ansible-lint==$ansible_lint_version pywinrm pywinrm[credssp]
+ansible --version
+ansible -m ping localhost
+```
+
+Install the ansible terraform dynamic inventory provider:
+
+```bash
+ansible_terraform_inventory_version='2.2.0' # see https://github.com/nbering/terraform-inventory/releases
+wget -q https://github.com/nbering/terraform-inventory/releases/download/v$ansible_terraform_inventory_version/terraform.py
+# make it use the python3 binary instead of just python.
+sed -i -E 's,#!.+,#!/usr/bin/python3,' terraform.py
+# fix the following error/warning:
+#   /etc/ansible/terraform.py:390: DeprecationWarning: 'encoding' is ignored and deprecated. It will be removed in Python 3.9   return json.loads(out_cmd, encoding=encoding)
+sed -i -E 's/return json.loads\(out_cmd, encoding=encoding\)/return json.loads(out_cmd)/g' terraform.py
+install -m 755 terraform.py .ansible-venv/terraform.py
+rm terraform.py
+```
+
+Create the infrastructure:
+
+```bash
+terraform init
+terraform plan -out=tfplan
+time terraform apply tfplan
+```
+
+**NB** if you have errors alike `Could not open '/var/lib/libvirt/images/terraform_example_root.img': Permission denied'` you need to reconfigure libvirt by setting `security_driver = "none"` in `/etc/libvirt/qemu.conf` and restart libvirt with `sudo systemctl restart libvirtd`.
+
+Show information about the libvirt/qemu guest:
+
+```bash
+virsh dumpxml terraform_example
+virsh qemu-agent-command terraform_example '{"execute":"guest-info"}' --pretty
+virsh qemu-agent-command terraform_example '{"execute":"guest-network-get-interfaces"}' --pretty
+./qemu-agent-guest-exec terraform_example winrm enumerate winrm/config/listener
+./qemu-agent-guest-exec terraform_example winrm get winrm/config
+```
+
+Configure the infrastructure:
+
+```bash
+# install the required collections.
+ansible-galaxy collection install -r ansible-collection-requirements.yml
+
+#ansible-doc -l # list all the available modules
+ansible-inventory --list --yaml
+ansible-lint playbook.yml
+ansible-playbook playbook.yml --syntax-check
+ansible-playbook playbook.yml --list-hosts
+
+# execute ad-hoc commands.
+ansible -vvv -m gather_facts all
+ansible -vvv -m win_ping all
+ansible -vvv -m win_command -a 'whoami /all' all
+ansible -vvv -m win_shell -a '$FormatEnumerationLimit = -1; dir env: | Sort-Object Name | Format-Table -AutoSize | Out-String -Stream -Width ([int]::MaxValue) | ForEach-Object {$_.TrimEnd()}' all
+
+# execute the playbook.
+# see https://docs.ansible.com/ansible/2.9/user_guide/windows_winrm.html#limitations
+# see https://docs.ansible.com/ansible/2.9/user_guide/windows_usage.html
+# see https://docs.ansible.com/ansible/2.9/modules/list_of_windows_modules.html
+# see https://docs.ansible.com/ansible/2.9/user_guide/windows_faq.html#can-i-run-python-modules-on-windows-hosts
+time ansible-playbook playbook.yml #-vvv
+```
+
+Destroy the infrastructure:
+
+```bash
+time terraform destroy -auto-approve
+```
+
+## WinRM
+
+Ansible should use the `winrm` `ansible_connection` type to manage Windows
+based hosts as its the native Windows management protocol.
+
+Its also advisable to use the `credssp` `ansible_winrm_transport` as its
+the most flexible transport:
+
+| ansible_winrm_transport | local accounts | active directory accounts | credentials delegation | encryption |
+|-------------------------|----------------|---------------------------|------------------------|------------|
+| basic                   | yes            | no                        | no                     | no         |
+| certificate             | yes            | no                        | no                     | no         |
+| kerberos                | no             | yes                       | yes                    | yes        |
+| ntlm                    | yes            | yes                       | no                     | yes        |
+| credssp                 | yes            | yes                       | yes                    | yes        |
+
+See https://docs.ansible.com/ansible/2.9/user_guide/windows_winrm.html#credssp
---
+++ main.tf
@@ -0,0 +1,189 @@
+# see https://github.com/hashicorp/terraform
+terraform {
+  required_version = "0.15.0"
+  required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/random
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+    # see https://registry.terraform.io/providers/hashicorp/template
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+    # see https://github.com/dmacvicar/terraform-provider-libvirt
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.3"
+    }
+    # see https://github.com/nbering/terraform-provider-ansible
+    # see https://registry.terraform.io/providers/nbering/ansible
+    ansible = {
+      source = "nbering/ansible"
+      version = "1.0.4"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+variable "prefix" {
+  default = "terraform_example"
+}
+
+variable "winrm_username" {
+  default = "vagrant"
+}
+
+variable "winrm_password" {
+  sensitive = true
+  # set the administrator password.
+  # NB the administrator password will be reset to this value by the cloudbase-init SetUserPasswordPlugin plugin.
+  # NB this value must meet the Windows password policy requirements.
+  #    see https://docs.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/password-must-meet-complexity-requirements
+  default = "HeyH0Password"
+}
+
+resource "ansible_host" "example" {
+  inventory_hostname = length(libvirt_domain.example.network_interface[0].addresses) > 0 ? libvirt_domain.example.network_interface[0].addresses[0] : ""
+  groups = [
+    "example"
+  ]
+}
+
+resource "ansible_group" "example" {
+  inventory_group_name = "example"
+  vars = {
+    ansible_user = var.winrm_username
+    ansible_password = var.winrm_password
+    ansible_connection = "winrm"
+    ansible_port = "5985"
+    # TODO use https and/or verify whether credssp enables encryption.
+    ansible_winrm_scheme = "http"
+    ansible_winrm_transport = "credssp"
+  }
+}
+
+# NB this generates a single random number for the cloud-init instance-id.
+resource "random_id" "example" {
+  byte_length = 10
+}
+
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/network.markdown
+resource "libvirt_network" "example" {
+  name = var.prefix
+  mode = "nat"
+  domain = "example.test"
+  addresses = ["10.17.3.0/24"]
+  dhcp {
+    enabled = true
+  }
+  dns {
+    enabled = true
+    local_only = false
+  }
+}
+
+# a multipart cloudbase-init cloud-config.
+# NB the parts are executed by their declared order.
+# see https://github.com/cloudbase/cloudbase-init
+# see https://cloudbase-init.readthedocs.io/en/1.1.2/userdata.html#cloud-config
+# see https://cloudbase-init.readthedocs.io/en/1.1.2/userdata.html#userdata
+# see https://www.terraform.io/docs/providers/template/d/cloudinit_config.html
+# see https://www.terraform.io/docs/configuration/expressions.html#string-literals
+data "template_cloudinit_config" "example" {
+  gzip = false
+  base64_encode = false
+  part {
+    filename = "enable-winrm-service-auth-credssp.ps1"
+    content_type = "text/x-shellscript"
+    content = <<-EOF
+      #ps1_sysnative
+      Set-StrictMode -Version Latest
+      $ErrorActionPreference = 'Stop'
+      $ProgressPreference = 'SilentlyContinue'
+      # wait for the winrm service to be ready.
+      while (!(Test-WSMan -ErrorAction SilentlyContinue)) {
+        Start-Sleep -Seconds 1
+      }
+      # enable credssp.
+      Enable-WSManCredSSP -Role Server -Force
+      EOF
+  }
+  part {
+    content_type = "text/cloud-config"
+    content = <<-EOF
+      #cloud-config
+      users:
+        - name: ${jsonencode(var.winrm_username)}
+          passwd: ${jsonencode(var.winrm_password)}
+          primary_group: Administrators
+          ssh_authorized_keys:
+            - ${jsonencode(trimspace(file("~/.ssh/id_rsa.pub")))}
+      EOF
+  }
+}
+
+# a cloudbase-init cloud-config disk.
+# NB this creates an iso image that will be used by the NoCloud cloudbase-init datasource.
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/cloudinit.html.markdown
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/libvirt/cloudinit_def.go#L133-L162
+resource "libvirt_cloudinit_disk" "example_cloudinit" {
+  name = "${var.prefix}_example_cloudinit.iso"
+  meta_data = jsonencode({
+    "instance-id": random_id.example.hex,
+  })
+  user_data = data.template_cloudinit_config.example.rendered
+}
+
+# this uses the vagrant windows image imported from https://github.com/rgl/windows-vagrant.
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/volume.html.markdown
+resource "libvirt_volume" "example_root" {
+  name = "${var.prefix}_root.img"
+  base_volume_name = "windows-2019-amd64_vagrant_box_image_0.img"
+  format = "qcow2"
+  size = 66*1024*1024*1024 # 66GiB. this root FS is automatically resized by cloudbase-init (by its cloudbaseinit.plugins.windows.extendvolumes.ExtendVolumesPlugin plugin which is included in the rgl/windows-vagrant image).
+}
+
+# a data disk.
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/volume.html.markdown
+resource "libvirt_volume" "example_data" {
+  name = "${var.prefix}_data.img"
+  format = "qcow2"
+  size = 6*1024*1024*1024 # 6GiB.
+}
+
+# see https://github.com/dmacvicar/terraform-provider-libvirt/blob/v0.6.3/website/docs/r/domain.html.markdown
+resource "libvirt_domain" "example" {
+  name = var.prefix
+  cpu = {
+    mode = "host-passthrough"
+  }
+  vcpu = 2
+  memory = 1024
+  video {
+    type = "qxl"
+  }
+  xml {
+    xslt = file("libvirt-domain.xsl")
+  }
+  qemu_agent = true
+  cloudinit = libvirt_cloudinit_disk.example_cloudinit.id
+  disk {
+    volume_id = libvirt_volume.example_root.id
+    scsi = false
+  }
+  disk {
+    volume_id = libvirt_volume.example_data.id
+    scsi = false
+  }
+  network_interface {
+    network_id = libvirt_network.example.id
+    wait_for_lease = true
+    hostname = "example"
+    addresses = ["10.17.3.2"]
+  }
+}
